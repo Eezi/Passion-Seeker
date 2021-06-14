07290e403812550b4bbe1169005c1610bd25c27e
Eezi <tomi.a.salo@outlook.com>
2021-06-14 20:35:59 +0300

    test answers go to backend correctly

--- backend/controllers/testAnswerController.js
+++ backend/controllers/testAnswerController.js
@@ -3,6 +3,13 @@ import TestAnswer from "../models/testAnswerModel.js";
 export default {
     getTestResults: async (parent, args, context) => {
         console.log('answers', args)
+        const { answers } = args;
+        TestAnswer.find({ 
+            $and: [
+                {'question1': {'$elemMatch': {'name': 'A1', 'state': 'non_free'} } },
+                {'question2': {'$elemMatch': {'name': 'B7', 'state': 'free'} } }
+           ]
+         })
         return 'testi'
     }
 }
