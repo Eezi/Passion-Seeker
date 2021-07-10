dcb8bc830d3624c45c006484089e3f6130b3efdf
Eezi <tomi.a.salo@outlook.com>
2021-07-10 23:01:53 +0300

    production changes

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ app.use(errorHandler);
 // HAndle production
 
 if(process.env.NODE_ENV === 'production'){
-  app.use(express.static(path.join(__dirname, '/frontend/dist/index.html'))) 
+  app.use(express.static(path.join(__dirname, '/frontend/dist'))) 
   app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html')))
 } else {
    app.get('/', (req, res) => {
