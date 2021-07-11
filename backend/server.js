e7c53421e52bc566de6c82f206dd3e9f4bfa5236
Eezi <tomi.a.salo@outlook.com>
2021-07-11 14:42:08 +0300

    production stuff

--- backend/server.js
+++ backend/server.js
@@ -21,18 +21,14 @@ app.use(notFound);
 
 app.use(errorHandler);
 
-//const __dirname = path.resolve();
+const __dirname = path.resolve();
 
 // HAndle production
-
 if(process.env.NODE_ENV === 'production'){
-  // Static folder
-  app.use(express.static(__dirname + '/public/'));
-
-  //Handle SPA
-  app.get('/.*/', (req, res) => res.sendFile(__dirname, '/public/index.html'));
+  app.use(express.static(path.join(__dirname, '/frontend/dist'))) 
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html')))
 } 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 
 server.listen().then(({ url }) => {
