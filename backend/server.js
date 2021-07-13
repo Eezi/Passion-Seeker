92113c33e8c86ce4ad6f918883e66d8f0aab88bf
Eezi <tomi.a.salo@outlook.com>
2021-07-13 20:01:38 +0300

    test

--- backend/server.js
+++ backend/server.js
@@ -22,11 +22,12 @@ const __dirname = path.resolve();
 
 // HAndle production
 if(process.env.NODE_ENV === 'production'){
-  app.use(serveStatic(__dirname + '/dist'));
+  //app.use(serveStatic(__dirname + '/dist'));
   //app.use(express.static(__dirname + '/public/'))
-  app.get('/.*/', (req, res) => res.sendFile(path.resolve(__dirname + '/dist/index.html')));
+  //app.get('/.*/', (req, res) => res.sendFile(path.resolve(__dirname + '/dist/index.html')));
   //app.use(express.static('public')); 
-  //app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html')))
+  app.use(express.static(path.join(__dirname, '/frontend/dist'))) 
+  app.get('/.*/', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html')))
    // Static folder
    /*app.use(express.static(__dirname + '/public/'));
 
