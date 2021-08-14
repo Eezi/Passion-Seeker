4f2bf1c2c62e3ac3c5d428fa24b38bdf4290bb51
Eezi <tomi.a.salo@outlook.com>
2021-08-14 16:01:31 +0300

    changes

--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,12 @@ server.applyMiddleware({ app });
 
 const __dirname = path.resolve();
 
+const PORT = process.env.PORT || 5000;
+
+app.listen({ port: process.env.PORT || 4000 }, () => {
+  console.log(`🚀  Server ready at ${process.env.PORT}`);
+});
+
 // HAndle production
 if(process.env.NODE_ENV === 'production'){
   //app.use('/', serveStatic(__dirname + '/client/build')) 
@@ -28,8 +34,3 @@ if(process.env.NODE_ENV === 'production'){
   app.use(express.static(path.join(__dirname, '/client/build'))) 
   app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
 };
-const PORT = process.env.PORT || 5000;
-
-app.listen({ port: process.env.PORT || 4000 }, () => {
-  console.log(`🚀  Server ready at ${process.env.PORT}`);
-});
